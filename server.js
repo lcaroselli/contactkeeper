47dabbfd0239dd7c5cfa9b0745b196867ed42131
Laura Caroselli <lcaroselli@outlook.com>
2021-06-25 20:12:19 -0500

    Connect to db

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the Contact Keeper API' })
 );
+
+// Connect Database
+connectDB();
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
