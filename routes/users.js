e8a5101f6f37e58ff489d47b04c6b83bd0b0db68
Laura Caroselli <lcaroselli@outlook.com>
2021-06-27 01:08:53 -0500

    Hashing and registering user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 const User = require('../models/Users');
@@ -20,13 +21,42 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      user = new User({
+        email,
+        name,
+        password,
+      });
+
+      // encrypt password we just created for the new user
+      const salt = await bcrypt.genSalt(10);
+
+      // take salt and hash password
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User saved');
+
+      // send back json web token to user
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
 
     // this is going to give me the data that is sent to the route
     // res.send(req.body);
