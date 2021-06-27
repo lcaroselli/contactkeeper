5e6f2353b565ef30f23802e497f542235b296f61
Laura Caroselli <lcaroselli@outlook.com>
2021-06-26 20:18:38 -0500

    Add user model

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator/check');
+
 const router = express.Router();
+const User = require('../models/Users');
 
 // instead of app.get, etc, can now just use router
 
@@ -7,8 +10,27 @@ const router = express.Router();
 // '/' here is equivalent to api/users
 // POST api/users
 // @access public
-router.post('/', (req, res) => {
-  res.send('Register a user');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Please add a Name').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('passed');
+
+    // this is going to give me the data that is sent to the route
+    // res.send(req.body);
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.get('/', (req, res) =>
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
